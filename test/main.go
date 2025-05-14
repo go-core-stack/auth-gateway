a0493734d1f0fb04b61fade43e7d2a09701c8c84
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-05-14 15:32:15 +0000

    Enable auth constructs and its validation
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

---
+++ test/main.go
@@ -0,0 +1,57 @@
+// Copyright © 2025 Prabhjot Singh Sethi, All Rights reserved
+// Author: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Prabhjot-Sethi/auth-gateway/pkg/keycloak"
+)
+
+func getUsername() string {
+	val, found := os.LookupEnv("Username")
+	if !found {
+		return "admin"
+	}
+	return val
+}
+
+func getPassword() string {
+	val, found := os.LookupEnv("Password")
+	if !found {
+		return "password"
+	}
+	return val
+}
+
+func getRealm() string {
+	val, found := os.LookupEnv("Realm")
+	if !found {
+		return "root"
+	}
+	return val
+}
+
+func main() {
+	client, err := keycloak.NewUserClient("http://localhost:8080", getRealm(), getUsername(), getPassword())
+	if err != nil {
+		log.Panicf("failed to get keycloak client: %s", err)
+	}
+
+	t, _ := client.GetAccessToken()
+	ticker := time.NewTicker(10 * time.Second)
+	fmt.Printf("Access token: %s\n", t)
+
+	for {
+		<-ticker.C
+		nt, _ := client.GetAccessToken()
+		if t != nt {
+			t = nt
+			fmt.Printf("\nAccess token: %s\n", t)
+		}
+	}
+}
