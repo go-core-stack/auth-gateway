5bbf44672481bbdbe9074ab031bbbd2999348500
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-07-25 11:52:03 +0530

    Handle Encryptor keys provided as part of env variable (#86)
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- pkg/table/encryptor.go
+++ pkg/table/encryptor.go
@@ -5,15 +5,30 @@ package table
 
 import (
 	"log"
+	"os"
 
 	"github.com/go-core-stack/core/utils"
+)
+
+const (
+	// Environment variable for encryptor key
+	EncryptorKeyEnvVar = "ENCRYPTOR_KEY"
+
+	// default value for encryptor key if not provided
+	// as part of environment
+	DefaultEncryptorKey = "MySuperSecretKey"
 )
 
 var encryptor utils.IOEncryptor
 
 func init() {
 	var err error
-	encryptor, err = utils.InitializeEncryptor("AuthTableInfra", "mydummykey")
+	key, ok := os.LookupEnv(EncryptorKeyEnvVar)
+	if !ok {
+		log.Printf("Warning: Encryptor Key not configured, switching to default key")
+		key = DefaultEncryptorKey
+	}
+	encryptor, err = utils.InitializeEncryptor("AuthTableInfra", key)
 	if err != nil {
 		log.Panicf("Failed to initialize encryptor: %s", err)
 	}
