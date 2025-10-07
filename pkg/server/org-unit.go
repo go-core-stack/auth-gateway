078ca5a32751874430f19861e12fa973b6676411
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-10-07 11:13:51 +0000

    Fix go fmt
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- pkg/server/org-unit.go
+++ pkg/server/org-unit.go
@@ -32,7 +32,7 @@ func (s *OrgUnitServer) ListOrgUnits(ctx context.Context, req *api.OrgUnitsListR
 		return nil, status.Errorf(codes.Unauthenticated, "User not authenticated")
 	}
 	OrgUnits, err := s.ouTable.FindByTenant(ctx, authInfo.Realm, "")
-	if err != nil && !errors.IsNotFound(err){
+	if err != nil && !errors.IsNotFound(err) {
 		log.Printf("got error while fetching org unit list for tenant %s: %s", authInfo.Realm, err)
 		return nil, status.Errorf(codes.Internal, "Something went wrong, Please try again later")
 	}
